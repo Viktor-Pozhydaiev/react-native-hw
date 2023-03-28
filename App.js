1b5ecaedabb20ff4d1be099b229e7a8f8149f975
Viktor <jude.peper@gmail.com>
2023-03-28 20:48:32 +0200

    adds styles to register form

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import {
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  View,
+} from "react-native";
 import { RegistrationPage, LoginScreen } from "./Screens/RegistrationScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground>
-        <RegistrationPage />
-        <LoginScreen />
+      <ImageBackground
+        style={styles.bgImg}
+        source={require("./assets/images/register-bg.png")}
+      >
+        <KeyboardAvoidingView>
+          <RegistrationPage />
+          {/* <LoginScreen /> */}
+        </KeyboardAvoidingView>
       </ImageBackground>
       <StatusBar style="auto" />
     </View>
@@ -18,5 +29,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#eaeaea",
+  },
+  bgImg: {
+    flex: 1,
+    resizeMode: "cover",
+    ...Platform.select({
+      ios: {
+        justifyContent: "center",
+      },
+      android: {
+        justifyContent: "flex-end",
+      },
+    }),
+    // alignItems: "center",
   },
 });
--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,93 +1,126 @@
-import { TouchableOpacity } from "react-native";
+import { useState } from "react";
 import {
   Text,
   View,
-  ImageBackground,
   StyleSheet,
   TextInput,
+  TouchableOpacity,
+  Keyboard,
 } from "react-native";
 
 export const RegistrationPage = () => {
+  const [isShowKey, setIsShowKey] = useState(false);
+
+  const toggleKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
   return (
-    <View>
-      <ImageBackground
-        style={styles.image}
-        source={require("../assets/images/register-bg.png")}
-      >
-        <View style={styles.form}>
-          <Text style={styles.formTitle}>Registration</Text>
-          <View>
-            <TextInput
-              style={styles.input}
-              textAlign={"left"}
-              placeholder={"Login"}
-            />
-          </View>
-          <View style={{ marginTop: 20 }}>
-            <TextInput
-              style={styles.input}
-              textAlign={"left"}
-              placeholder={"Email"}
-            />
-          </View>
-          <View style={{ marginTop: 20 }}>
-            <TextInput
-              style={styles.input}
-              secureTextEntry={true}
-              textAlign={"left"}
-              placeholder={"Password"}
-            />
-          </View>
-          <TouchableOpacity style={styles.btn}>
-            <Text>Register up</Text>
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
+    <View style={{ ...styles.form, paddingBottom: isShowKey ? 45 : 100 }}>
+      <View style={styles.avatar}></View>
+      <Text style={styles.formTitle}>Реєстрація</Text>
+      <View style={styles.logInput}>
+        <TextInput
+          style={styles.input}
+          textAlign={"left"}
+          placeholder={"Логін"}
+          onFocus={() => setIsShowKey(true)}
+        />
+      </View>
+      <View style={styles.emailInput}>
+        <TextInput
+          style={styles.input}
+          textAlign={"left"}
+          placeholder={"Адреса електронної пошти"}
+          onFocus={() => setIsShowKey(true)}
+        />
+      </View>
+      <View style={styles.passInput}>
+        <TextInput
+          style={styles.input}
+          textAlign={"left"}
+          placeholder={"Пароль"}
+          secureTextEntry={true}
+          onFocus={() => setIsShowKey(true)}
+        />
+      </View>
+      <View>
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.regBtn}
+          onPress={toggleKeyboard}
+        >
+          <Text style={styles.btnTitle}>Реєстрація</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.linkTitle}>Вже є обліковий запис? Увійти</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "aff",
-  },
-
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    width: 415,
-    height: 812,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#8a2be2",
-    height: 50,
-    borderRadius: 6,
-    color: "#f0f8ff",
+  avatar: {
+    position: "absolute",
+    width: 120,
+    height: 120,
+    left: 138,
+    backgroundColor: "#f6f6f6",
+    borderRadius: 16,
   },
   form: {
-    position: "absolute",
-    backgroundColor: "#fffff",
-    marginHorizontal: 40,
+    bottom: 0,
+    margin: 0,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: "#ffffff",
   },
   formTitle: {
-    height: 36,
+    paddingTop: 32,
+    paddingBottom: 33,
     fontWeight: 500,
     fontSize: 30,
+    lineHeight: 35,
+    letterSpacing: 0.01,
     textAlign: "center",
-    color: "#212121",
   },
-  btn: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    gap: 12,
+  input: {
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "#e8e8e8",
+    height: 50,
+    borderRadius: 10,
+    color: "#212121", //#212121
+    marginHorizontal: 16,
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  emailInput: {
+    marginTop: 16,
+  },
+  passInput: {
+    marginTop: 16,
+    marginBottom: 32,
+  },
+  regBtn: {
+    display: "flex",
+    justifyContent: "center",
+    marginHorizontal: 16,
     height: 51,
-    left: 16,
-    right: 16,
-    bottom: 113,
-    backgroundColor: "#ffa500",
     borderRadius: 100,
+    backgroundColor: "#ff6c00",
+  },
+  btnTitle: {
+    fontWeight: 400,
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "center",
+    color: "#ffffff",
+  },
+  linkTitle: {
+    paddingTop: 16,
+    textAlign: "center",
+    fontWeight: 400,
+    fontSize: 16,
+    lineHeight: 19,
   },
 });
